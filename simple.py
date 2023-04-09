b65734b3bb95c913b196141668a1893a40d47c0a
asapacsin <50076847+asapacsin@users.noreply.github.com>
2023-04-09 23:14:20 +0800

    Add files via upload

--- simple.py
+++ simple.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import os
 from PIL import Image
 import numpy
 import time
@@ -30,65 +29,68 @@ def new_gram_matrix(input):
     num_features = tf.cast(shape[0]*shape[1],tf.float32)
     return gram/num_features
 
-def styleContentLoss(image,target_style,target_content):
-    vgg = vgg16(style_layers+content_layers)
-    new_output = vgg(image)
-    new_style_output,new_content_output = new_output[:len(style_layers)],new_output[len(style_layers):]
-    new_style_output = [new_gram_matrix(style) for style in new_style_output]
+def styleContentLoss(output_dict,target_style,target_content):
+    new_style_output,new_content_output = output_dict['style'],output_dict['content']
     style_loss = tf.add_n([tf.reduce_mean((new_style-style)**2)/2 for new_style,style in zip(new_style_output,target_style)])*style_weight
     content_loss = tf.add_n([tf.reduce_mean((new_content-content)**2)/2 for new_content,content in zip(new_content_output,target_content)])*content_weight
     loss = style_loss + content_loss
     return loss
 
+def variation_loss(output_image,):
+    x_delta = output_image[:,:,1:,:] - output_image[:,:,:-1,:]
+    y_delta = output_image[:,1:,:,:] - output_image[:,:-1,:,:]
+    loss = (tf.reduce_mean(x_delta**2) + tf.reduce_mean(y_delta**2))*variation_weight
+    return loss
+
 def train(output_image,target_style,target_content,opt):
      with tf.GradientTape() as tape:
-            loss = styleContentLoss(output_image,target_style,target_content)
-            x_delta = output_image[:,:,1:,:] - output_image[:,:,:-1,:]
-            y_delta = output_image[:,1:,:,:] - output_image[:,:-1,:,:]
-            loss += (tf.reduce_mean(x_delta**2) + tf.reduce_mean(y_delta**2))*variation_weight
+            output_dict = extrator(output_image)
+            loss = styleContentLoss(output_dict,target_style,target_content)
+            loss += variation_loss(output_image)
      grad = tape.gradient(loss,output_image)
      opt.apply_gradients([(grad,output_image)])
      output_image.assign(tf.clip_by_value(output_image,clip_value_min=0.0,clip_value_max=1.0))
 
-def style(content_path,style_path,output_path):
-    vgg = vgg16(style_layers+content_layers)
-   
-    style_image = Image.open(style_path)
-    content_image = Image.open(content_path)
-    #convert content_image as rgb
-    content_image = content_image.convert('RGB')
-    #convert style_image as rgb
-    style_image = style_image.convert('RGB')
-    style_image = asarray(style_image)
-    content_image = asarray(content_image)
-    style_image = tf.convert_to_tensor(style_image)
-    content_image = tf.convert_to_tensor(content_image)
+def process_img(img_path):
+    img = Image.open(img_path)
+    img = img.convert('RGB')
+    img = asarray(img)
+    img = tf.convert_to_tensor(img)
     new_shape = tf.cast([width,height],tf.int32)
-    style_image = tf.image.resize(style_image,new_shape)
-    content_image = tf.image.resize(content_image,new_shape)
-    style_image = style_image[tf.newaxis,:]/255
+    img = tf.image.resize(img,new_shape)
+    img = img[tf.newaxis,:]/255
+    return img
 
-    content_image = content_image[tf.newaxis,:]/255
+class styleContentModel(tf.keras.models.Model):
+    def __init__(self):
+        super(styleContentModel,self).__init__()
+        self.vgg = vgg16(style_layers+content_layers)
+    def call(self,image):
+        preprocess_input = tf.keras.applications.vgg16.preprocess_input(image*255)
+        output = self.vgg(preprocess_input)
+        style_output,content_output = output[:len(style_layers)],output[len(style_layers):]
+        style_output = [new_gram_matrix(style) for style in style_output]
+        output_dict = {'style':style_output,'content':content_output}
+        return output_dict
+
+extrator = styleContentModel()
+
+def style(content_path,style_path,output_path):
+    style_image = process_img(style_path)
+    content_image = process_img(content_path)
     
     
     #initialize the target 
-    output = vgg(content_image)
-    style_output = vgg(style_image)
-    photo_style_output,photo_content_output = output[:len(style_layers)],output[len(style_layers):]
-    style_output = style_output[:len(style_layers)]
+    content_dict = extrator(content_image)
+    style_dict = extrator(style_image)
+    target_content = content_dict['content']
+    target_style = style_dict['style']
 
-    #process the style output by gram matrix
-    photo_style_output = [new_gram_matrix(style) for style in photo_style_output]
-    style_output = [new_gram_matrix(style) for style in style_output]
 
-    #initialize the target image
-    target_content = photo_content_output
-    target_style = style_output
 
     output_image = tf.Variable(content_image)
 
     #do the gradient descent
-    os.makedirs('output',exist_ok=True)
     opt = tf.optimizers.Adam(learning_rate=0.02,beta_1=0.99,epsilon=1e-1)
     current_time = time.time()
     local_time = time.localtime(current_time)
